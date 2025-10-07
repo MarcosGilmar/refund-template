762d7caeefaf5b3bca613da9048fc4ce72a871ea
marcosgilmar <marcosgilmarvalentini@gmail.com>
2025-10-07 13:55:30 -0300

    adding javascript and creating features

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
-const form = document.querySelector('form');
-const amount = document.getElementById('amount');
-const expense = document.getElementById('expense');
-const category = document.getElementById('category');
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
+//unordered list elements
+const expenseList = document.querySelector("ul")
+const expensesQuantity = document.querySelector("aside header p span")
+const expensesTotal = document.querySelector("aside header h2")
 
 //input event
 amount.oninput = () => {
@@ -35,11 +40,97 @@ form.onsubmit = (event) => {
     expenseAdd(newExpense)
 }
 
+//add item to the list
 function expenseAdd(newExpense) {
     try {
+        const expenseItem = document.createElement('li') //create an li
+        expenseItem.classList.add('expense-item') // add class to the li
 
-    } catch (error) {
+        const expenseIcon = document.createElement('img') //create the tag img
+        expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`) //attribute the source of the image
+        expenseIcon.setAttribute("alt", newExpense.category_name) //attribute the alternative text of the image
+
+        const expenseInfo = document.createElement('div') 
+        expenseInfo.classList.add('expense-info') 
+
+        const expenseName = document.createElement('strong') //create expense name
+        expenseName.textContent = newExpense.expense
+
+        const expenseCategory = document.createElement('span') //create expense category
+        expenseCategory.textContent = newExpense.category_name
+
+        expenseInfo.append(expenseName, expenseCategory) //add expenseName and expenseCategory as children of expenseInfo
+
+        const expenseAmount = document.createElement("span") //create expense amount
+        expenseAmount.classList.add("expense-amount")
+        expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
+
+        const removeIcon = document.createElement("img")
+        removeIcon.classList.add("remove-icon")
+        removeIcon.setAttribute("src", "img/remove.svg")
+        removeIcon.setAttribute("alt", "Remover")
+
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon) //add all elements as children of expenseItem
+        expenseList.append(expenseItem) //add expenseItem as a child of expenseList
+
+        formClear() //clear the form
+        updateTotals()
+
+        } catch (error) {
         alert("Não foi possível adicionar a despesa.")
         console.error(error)
     }
+}
+
+//update totals
+function updateTotals() {
+    try {
+        const items = expenseList.children //get all list items
+        
+        expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}` 
+
+        let total = 0
+
+        for(let item = 0; item < items.length; item++) {
+            const itemAmount = items[item].querySelector(".expense-amount")
+
+            let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",",".") //remove non-numeric characters and replace comma with dot
+
+            value = parseFloat(value) //convert to float
+
+            if(isNaN(value)) { //check if value is a number
+                return alert("Valor inválido")
+            }
+            total += Number(value) 
+        }
+
+        const symbolBRL = document.createElement("small")
+        symbolBRL.textContent = "R$"
+
+        total = formatCurrencyBRL(total).toUpperCase().replace("R$", "") //format total
+
+        expensesTotal.innerHTML = "" //clear the total before updating
+        expensesTotal.append(symbolBRL, total) //update total
+
+    } catch (error) {
+        console.log(error)
+        alert("Não foi possível atualizar os totais.")
+    }
+}
+
+expenseList.addEventListener("click", function(event) {
+    if(event.target.classList.contains("remove-icon")) {
+        const item = event.target.closest(".expense") //obtain the closest parent with class "expense"
+
+        item.remove() //remove the item from the list
+    }
+    updateTotals() //update totals after removal
+})
+
+function formClear() {
+    expense.value = ""
+    category.value = ""
+    amount.value = ""
+
+    expense.focus() //focus on the expense input
 }
